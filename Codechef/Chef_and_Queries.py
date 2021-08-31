c3e76ae92e34a5567c9d87389ca66653e28c3a70
Taaha Multani <79622420+taaaahahaha@users.noreply.github.com>
2021-08-31 10:39:28 +0530

    Add files via upload

---
+++ Codechef/Chef_and_Queries.py
@@ -0,0 +1,8 @@
+n,no_q = map(int,input().strip().split())
+Ai = [int(i) for i in input().split()]
+for i in range(no_q):
+    li = [int(x) for x in input().split()]
+    if li[0]==1 : 
+    	for i in range(li[1],li[2]+1):
+        	Ai[i-1]  += (li[3]+i-li[1])**2
+    elif li[0]==2 : print(Ai[li[1]-1])
