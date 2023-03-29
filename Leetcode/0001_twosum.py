f528466f92e15fb16eda1724e20a1ad87c39d397
Taaha Multani <taaha.m@somaiya.edu>
2023-03-30 01:55:25 +0530

    .

--- Leetcode/0001_twosum.py
+++ Leetcode/0001_twosum.py
@@ -1,11 +1,27 @@
+# Taaha Multani @ https://github.com/taaaahahaha
+# Title: 0001_twosum.py
+# Date: 2023-03-29 20:29:37
+
+ii = lambda: int(input())
+imi = lambda: [int(i) for i in input().split()]
+
+# O(n^2)
+
 class Solution:
-    
-    def twoSum(self, nums, target):
-            values = {}
-            for i, num in enumerate(nums):
-                remaining = target - num
-                if remaining in values:
-                    return [i, values[remaining]]
-                else:
-                    values[num] = i
-     
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+
+# O(n)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        d = {}
+        for i in range(len(nums)):
+            if target-nums[i] in d:
+                return [d[target-nums[i]],i]
+            d[nums[i]] = i
